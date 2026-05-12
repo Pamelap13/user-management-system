1dfa94069245f62db60c5da7ffd68c3f4283857d
Pamela <pamela@MacBook-Pro-de-Pamela.local>
2026-05-11 20:58:12 -0500

    feat: add functional interfaces and stream processing examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,10 @@ import org.example.repository.InMemoryUserRepository;
 import org.example.repository.UserRepository;
 import org.example.service.UserService;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -30,6 +34,14 @@ public class Main {
         );
         userService.findByEmail("luis@email.com")
                 .ifPresent(u-> System.out.println("\nfound: " + u.getName()));
+
+        User user = new User(1L, "Pamela", "pamela@email.com", true);
+        Predicate<User> isActiveUser = u -> u.isActive();
+        Function<User,String> getEmail = u -> u.getEmail();
+        Consumer<User> printName = u -> System.out.println(u.getName());
+        System.out.println(isActiveUser.test(user));
+        System.out.println(getEmail.apply(user));
+        printName.accept(user);
     }catch (IllegalArgumentException e){
         System.out.println(e.getMessage());
     }
